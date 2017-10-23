5daa55e4cd774fc9fe5e8e016923eac48961b190
Daron Adkins <daron.adkins@gmail.com>
2017-10-23 10:01:55 -0500

    functioning jobs

---
+++ app/Jobs/UpdateEcar.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Listing;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class UpdateEcar implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $listing = new Listing();
+
+        $listing->updateEcarListings();
+    }
+}
