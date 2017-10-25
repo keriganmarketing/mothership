63d0b5bc9b46a1d3679082adfd15ca0e3477f884
Daron Adkins <daron.adkins@gmail.com>
2017-10-25 10:38:58 -0500

    wip

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -32,7 +32,7 @@ class Kernel extends ConsoleKernel
                 new UpdateEcar,
                 new CleanBcar
             ])->dispatch();
-        })->hourlyAt(40);
+        })->hourlyAt(9);
     }
 
     /**
