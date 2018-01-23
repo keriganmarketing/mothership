6553ec764225128078069358a46ee178b50b0d43
Daron Adkins <daron.adkins@gmail.com>
2018-01-23 08:40:44 -0600

    wip

--- app/Listing.php
+++ app/Listing.php
@@ -177,6 +177,7 @@ class Listing extends Model
         }
         $listings = Listing::whereIn('listing_member_shortid', $ids)
             ->orWhereIn('colisting_member_shortid', $ids)
+            ->latest()
             ->groupBy('full_address')
             ->get();
 
