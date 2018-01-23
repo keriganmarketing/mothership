46df2e3d6b3d0f4c388ec5f8ac5f3c5781a6fd54
Daron Adkins <daron.adkins@gmail.com>
2018-01-23 08:59:39 -0600

    wip

--- app/Listing.php
+++ app/Listing.php
@@ -177,7 +177,8 @@ class Listing extends Model
         }
         $listings = Listing::whereIn('listing_member_shortid', $ids)
             ->orWhereIn('colisting_member_shortid', $ids)
-            ->groupBy('price', 'full_address')
+            ->groupBy('full_address')
+            ->having('association', 'ecar')
             ->get();
 
         ProcessListingImpression::dispatch($listings);
