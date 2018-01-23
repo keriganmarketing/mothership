7be28ab0254aea4ccda0063be68bbef4b517a723
Daron Adkins <daron.adkins@gmail.com>
2018-01-23 08:41:35 -0600

    wip

--- app/Listing.php
+++ app/Listing.php
@@ -177,7 +177,7 @@ class Listing extends Model
         }
         $listings = Listing::whereIn('listing_member_shortid', $ids)
             ->orWhereIn('colisting_member_shortid', $ids)
-            ->latest()
+            ->oldest()
             ->groupBy('full_address')
             ->get();
 
