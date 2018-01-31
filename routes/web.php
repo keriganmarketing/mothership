c19544b6696ff017b2960071fdaeb2f84d1169f8
Daron Adkins <daron.adkins@gmail.com>
2018-01-31 16:38:22 -0600

    wip

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::prefix('api/v1')->group(function () {
     Route::get('search', 'ListingsSearchController@index');
     Route::get('listing/{mlsNumber}', 'ListingController@show');
     Route::get('listings', 'ListingController@index');
-    Route::get('omnibar', 'OmnibarController@create');
+    Route::any('omnibar', 'OmnibarController@create');
     Route::get('allMapListings', 'MapSearchController@index');
     Route::get('agentlistings', 'AgentListingsController@index');
     Route::get('agents', 'AgentSearchController@index');
