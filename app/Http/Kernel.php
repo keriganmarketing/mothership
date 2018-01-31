0774e7f19ac6a29b33b4680c169451d7d2085318
Daron Adkins <daron.adkins@gmail.com>
2018-01-31 16:35:43 -0600

    wip

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -55,7 +55,7 @@ class Kernel extends HttpKernel
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
-        'cors' => \App\Http\Middleware\Cors::class,
+        'cors' => \Spatie\Cors\Cors::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
     ];
