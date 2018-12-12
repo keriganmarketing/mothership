9d0e532308dae98e9106a54a231004fd63f83d66
Bryan Baird <bryan@kerigan.com>
2018-12-12 16:59:05 -0500

    adjuist redis config

--- config/queue.php
+++ config/queue.php
@@ -60,7 +60,7 @@ return [
         'redis' => [
             'driver' => 'redis',
             'connection' => 'default',
-            'queue' => 'default',
+            'queue' => ['updaters','cleaners','default'],
             'retry_after' => 60,
         ],
 
