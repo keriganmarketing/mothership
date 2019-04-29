b76788f118d09120ebe432bf41fdf299e6ea0866
Bryan Baird <bryan@kerigan.com>
2019-04-29 09:16:54 -0400

    adjust tags

--- app/Jobs/ProcessListingClick.php
+++ app/Jobs/ProcessListingClick.php
@@ -59,6 +59,6 @@ class ProcessListingClick implements ShouldQueue
      */
     public function tags()
     {
-        return ['click', $this->$listing->mls_account, $this->userAgent];
+        return ['click', $this->userAgent];
     }
 }
