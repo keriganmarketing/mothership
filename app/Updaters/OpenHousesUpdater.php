a1fe7c930b4b141e14f8ca40d41e5b6b9ba2091c
Bryan Baird <bryan@kerigan.com>
2020-10-23 16:54:33 -0400

    add output to open house updater

--- app/Updaters/OpenHousesUpdater.php
+++ app/Updaters/OpenHousesUpdater.php
@@ -16,11 +16,12 @@ class OpenHousesUpdater extends Updater implements MakesUpdates
         parent::__construct();
     }
 
-    public function update()
+    public function update($output = false)
     {
         $lastModified = $this->getLastModifiedDate('open_houses');
         $results = $this->getNewOpenHouses($lastModified);
         foreach ($results as $result) {
+            echo ($output ? '|' : null);
             $this->handleResult($result);
         }
         OpenHouse::syncWithListings();
